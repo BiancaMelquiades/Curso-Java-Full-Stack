d71fcbcee97d41523cb27eb3fc78b1dd837c9cd3
BiancaMelquiades <biancamelquiadesbia@gmail.com>
2023-04-12 15:11:47 -0300

    Exercício Hello World/ Banco de dados/ Spring Boot

---
+++ 5. Banco de Dados/Hello World/main/java/org/generation/helloworld/controller/HelloController.java
@@ -0,0 +1,24 @@
+package org.generation.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//indica para o spring que o código abaixo vai ser um Controller, ou umcontrolador da nossa API
+@RestController
+
+//indica o endpoint da nossa classe controladora
+@RequestMapping("/hello")
+public class HelloController {
+
+	@GetMapping
+	public String hello() {
+		return "Alô mundão!";
+	}
+	
+	@GetMapping("/bsm")
+	public String bsm() {
+		return "Trabalho em equipe";
+	}
+	
+}
